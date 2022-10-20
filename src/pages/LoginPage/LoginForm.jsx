e4d6e0d0991f1cefe14ed2a74b7485d40e3a2b55
Paweł Potempa <pwpotempa@gmail.com>
2022-10-20 15:02:53 +0200

    test

--- src/pages/LoginPage/LoginForm.jsx
+++ src/pages/LoginPage/LoginForm.jsx
@@ -1,34 +1,73 @@
-import css from './LoginForm.module.scss';
+import css from "./LoginForm.module.scss";
 import Panel from "../../components/Panel";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import warningIcon from "../../assets/warningIcon.svg";
 
-function LoginForm({onLoginAttempt}) {
-
-  // TODO: Your code.
-  //       When user clicks "log in" in the form below, You should call function onLoginAttempt(username,password)
-  //       which is provided here as a prop.
-
+function LoginForm({ warning, handleChange, handleSubmit }) {
   return (
     <Panel className={css.LoginForm}>
-
-      // TODO: Modify this as needed
-
-      <form>
-        <fieldset>
-          <legend>Log in</legend>
+      <form className={css.formWrapper} onSubmit={handleSubmit} noValidate>
+        <fieldset className={css.inputWrapper}>
+          <legend className={css.formTitle}>Log in</legend>
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" />
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
-          <input type="submit" id="submit" value="Log in" />
+          <input
+            required={true}
+            name="username"
+            className={`${css.input} ${warning.username && css.error}`}
+            type="text"
+            id="email"
+            placeholder="Enter email..."
+            onChange={handleChange}
+          />
+          {warning.username && (
+            <div className={css.errorContainer}>
+              <img src={warningIcon} alt="" />
+              <span className={css.errorMessage}>{warning?.username}</span>
+            </div>
+          )}
+          <div className={css.passwordContainer}>
+            <label htmlFor="password">Password</label>
+            <Link className={css.link} tabIndex="-1">
+              Forgot your password?
+            </Link>
+          </div>
+          <input
+            required={true}
+            name="password"
+            className={`${css.input} ${warning.password && css.error}`}
+            type="password"
+            id="password"
+            placeholder="Enter password..."
+            onChange={handleChange}
+          />
+          {warning.password && (
+            <div className={css.errorContainer}>
+              <img src={warningIcon} alt="" />
+              <span className={css.errorMessage}>{warning?.password}</span>
+            </div>
+          )}
+          <input
+            className={css.submitButton}
+            type="submit"
+            id="submit"
+            value="Log in"
+          />
         </fieldset>
+        <hr className={css.divider}></hr>
+        <p className={css.text}>Don't have an account yet?</p>
+        <Link className={css.link}>
+          <p>Create an account</p>
+        </Link>
       </form>
     </Panel>
-  )
+  );
 }
 
 LoginForm.propTypes = {
-  onLoginAttempt: PropTypes.func.isRequired
-}
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  warning: PropTypes.object.isRequired,
+};
 
 export default LoginForm;
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,43 +1,94 @@
-import css from './index.module.scss';
+import css from "./index.module.scss";
 import DefaultLayout from "../../layouts/DefaultLayout";
 import LoginForm from "./LoginForm";
 import useAxios from "../../api/useAxios";
 import useDatastore from "../../datastore/useDatastore";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function LoginPage() {
   const axios = useAxios();
-  const {setAccessToken, setUser} = useDatastore();
+  const { setAccessToken, setUser, addMessage, removeMessage } = useDatastore();
   const navigate = useNavigate();
+  const [values, setValues] = useState({ username: "", password: "" });
+  const [warning, setWarning] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    addMessage("error", error);
+  }, [error]);
 
   function onLoginAttempt(username, password) {
-    axios.post('/api/users/login', {
-      username,
-      password
-    }).then((response) => {
-
-      if (response.status === 200) {
-        setAccessToken(response.data.token);
-        setUser(response.data.user);
-        navigate('/')
-      }
-
-    }).catch((error) => {
-
-      // TODO: catch errors and pass appropriate feedback to user.
-
-    });
+    axios
+      .post("/api/users/login", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setAccessToken(response.data.token);
+          setUser(response.data.user);
+          removeMessage("error", error);
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          if (error.response.status === 401) {
+            setWarning({
+              username: "Invalid username or password",
+              password: "Invalid username or password",
+            });
+          } else {
+            setError("Server is offline, try again later");
+          }
+        } else if (error.request) {
+          setError("Access denied, try again later");
+        } else {
+          setError("Something went wrong, try again later");
+        }
+      });
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (values.username.length > 0 && values.password.length > 0) {
+      onLoginAttempt(values.username, values.password);
+    } else if (values.username.length === 0 && values.password.length === 0) {
+      setWarning({
+        username: "Username is required",
+        password: "Password is required",
+      });
+    } else if (values.username.length === 0) {
+      setWarning({
+        username: "Username is required",
+        password: "",
+      });
+    } else if (values.password.length === 0) {
+      setWarning({
+        username: "",
+        password: "Password is required",
+      });
+    }
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
   return (
     <DefaultLayout>
       <div className={css.alignmentWrapper}>
         <LoginForm
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
           onLoginAttempt={onLoginAttempt}
+          warning={warning}
+          setWarning={setWarning}
         />
       </div>
     </DefaultLayout>
-  )
+  );
 }
 
 export default LoginPage;
